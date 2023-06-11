ef06772fe872cfe03e8cfbf9c571be41af198ba6
Mazharul Islam <mie.mazharul@gmail.com>
2023-06-11 10:15:09 +0600

    feat: add news api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,37 @@
-import {Inter} from 'next/font/google'
 import Head from "next/head";
+import {GetServerSideProps} from "next";
+import {NewsApiResponse, NewsArticle} from "@/models/NewsArticle";
 
-const inter = Inter({subsets: ['latin']})
+interface BreakingNewsPageProps {
+    newsArticles: NewsArticle[],
+}
 
-export default function BreakingNewsPage() {
+export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
+    const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=' +
+        process.env.NEWS_API_KEY,
+    );
+    const newsResponse: NewsApiResponse = await response.json();
+    return {
+        props: {
+            newsArticles: newsResponse.articles,
+        }
+    }
+}
+export default function BreakingNewsPage({newsArticles}: BreakingNewsPageProps) {
     return (
         <>
             <Head>
                 <title key="title">Breaking News - NextJS News App</title>
             </Head>
             <main>
-                <h1>Hello world</h1>
+                <h1>Breaking News</h1>
+                <ul>
+                    {newsArticles.map((article) => (
+                        <li key={article.url}>
+                            <a href={article.url}>{article.title}</a>
+                        </li>
+                    ))}
+                </ul>
             </main>
         </>
     )
